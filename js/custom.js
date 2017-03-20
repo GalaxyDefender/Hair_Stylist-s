b2675cc70fe8ef97d1764b1dd0a7fc67881d3a4b
GalaxyDefender <qgerard.gerard@gmail.com>
2017-03-20 15:17:50 +0200

    added example of using Angular with nav

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-    $('.navbar-nav li').click(function(e){
-       $('.navbar-nav li.active').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
+//$(document).ready(function(){
+//    $('.navbar-nav li').click(function(e){
+//       $('.navbar-nav li.active').removeClass('active');
+//        $(this).addClass('active');
+//        e.preventDefault();
+//    });
+//});
+
+var j = document.getElementsByClassName("nav");
+var x = j.getElementsByTagName("li");
+
+x.addEventListener("click", function(){
+   document.getElementsByClassName("nav").style.color = "purple"; 
 });
